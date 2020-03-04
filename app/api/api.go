2eba13e5449efe8c2d87fd62215ce8eabb4116d2
luodan@elinkport.com <linximeng>
2020-03-04 15:36:50 +0800

    注释api

--- app/api/api.go
+++ app/api/api.go
@@ -1,18 +1,16 @@
 package api
 
 import (
-	"github.com/kataras/iris/v12"
 	"github.com/qor/admin"
 	"github.com/qor/qor"
 	"go-tenancy/config/application"
 	"go-tenancy/config/db"
-	"go-tenancy/models/tenant"
 )
 
 // New new api app
 func New(config *Config) *App {
 	if config.Prefix == "" {
-		config.Prefix = "/v1/admin"
+		config.Prefix = "/api"
 	}
 	return &App{Config: config}
 }
@@ -29,14 +27,14 @@ type Config struct {
 
 // ConfigureApplication configure application
 func (app App) ConfigureApplication(application *application.Application) {
-	API := admin.New(&qor.Config{DB: db.DB})
+	_ = admin.New(&qor.Config{DB: db.DB})
 
-	API.AddResource(&tenant.RabcUser{})
+	//API.AddResource(&tenant.RabcUser{})
 	//API.AddResource(&rabc.Role{})
 	//API.AddResource(&rabc.Permission{})
 	//API.AddResource(&rabc.OauthToken{})
 
-	std := iris.FromStd(API.NewServeMux(app.Config.Prefix))
-	application.IrisApp.Any(app.Config.Prefix, std)
+	//std := iris.FromStd(API.NewServeMux(app.Config.Prefix))
+	//application.IrisApp.Any(app.Config.Prefix, std)
 
 }
