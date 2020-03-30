b428d9a797bd30e8cfccc4820f8cb43bb01acedf
snowly <BrendenAudrina6287@gmail.com>
2020-03-30 18:06:48 +0800

    增加驱动
    Signed-off-by: snowly <BrendenAudrina6287@gmail.com>

--- services/perm_test.go
+++ services/perm_test.go
@@ -7,6 +7,9 @@ import (
 	"testing"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/snowlyg/go-tenancy/models"
 )
 
