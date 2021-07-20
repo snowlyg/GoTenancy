03ef9efb6f0d4c91adbcc3068eae6da4f0c616ee
snowlyg <569616226@qq.com>
2021-07-20 14:04:07 +0800

    增加单元测试覆盖

--- tests/client_auto_label_test.go
+++ tests/client_auto_label_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 )
 
-func TestClientUserLabelList(t *testing.T) {
+func TestClientAutoList(t *testing.T) {
 	auth := tenancyWithLoginTester(t)
 	defer baseLogOut(auth)
-	obj := auth.POST("v1/merchant/userLabel/getLabelList").
+	obj := auth.POST("v1/merchant/userLabel/auto/getLabelList").
 		WithJSON(map[string]interface{}{"page": 1, "pageSize": 10}).
 		Expect().Status(http.StatusOK).JSON().Object()
 	obj.Keys().ContainsOnly("status", "data", "message")
@@ -28,13 +28,13 @@ func TestClientUserLabelList(t *testing.T) {
 	first.Keys().ContainsOnly("id", "labelName", "type", "sysTenancyId", "createdAt", "updatedAt")
 }
 
-func TestClientUserLabelProcess(t *testing.T) {
+func TestClientAutoProcess(t *testing.T) {
 	data := map[string]interface{}{
 		"labelName": "sdfsdfs34234",
 	}
 	auth := tenancyWithLoginTester(t)
 	defer baseLogOut(auth)
-	obj := auth.POST("v1/merchant/userLabel/createUserLabel").
+	obj := auth.POST("v1/merchant/userLabel/auto/createUserLabel").
 		WithJSON(data).
 		Expect().Status(http.StatusOK).JSON().Object()
 	obj.Keys().ContainsOnly("status", "data", "message")
@@ -49,31 +49,15 @@ func TestClientUserLabelProcess(t *testing.T) {
 		update := map[string]interface{}{
 			"labelName": "sdfsdfs213213",
 		}
-		obj = auth.PUT(fmt.Sprintf("v1/merchant/userLabel/updateUserLabel/%d", int(userLabelId))).
+		obj = auth.PUT(fmt.Sprintf("v1/merchant/userLabel/auto/updateUserLabel/%d", int(userLabelId))).
 			WithJSON(update).
 			Expect().Status(http.StatusOK).JSON().Object()
 		obj.Keys().ContainsOnly("status", "data", "message")
 		obj.Value("status").Number().Equal(200)
 		obj.Value("message").String().Equal("更新成功")
-		userLabel = obj.Value("data").Object()
-
-		userLabel.Value("id").Number().Ge(0)
-		userLabel.Value("labelName").String().Equal(update["labelName"].(string))
-
-		obj = auth.GET("v1/merchant/userLabel/getCreateUserLabelMap").
-			Expect().Status(http.StatusOK).JSON().Object()
-		obj.Keys().ContainsOnly("status", "data", "message")
-		obj.Value("status").Number().Equal(200)
-		obj.Value("message").String().Equal("获取成功")
-
-		obj = auth.GET(fmt.Sprintf("v1/merchant/userLabel/getUpdateUserLabelMap/%d", int(userLabelId))).
-			Expect().Status(http.StatusOK).JSON().Object()
-		obj.Keys().ContainsOnly("status", "data", "message")
-		obj.Value("status").Number().Equal(200)
-		obj.Value("message").String().Equal("获取成功")
 
 		// deleteUserLabel
-		obj = auth.DELETE(fmt.Sprintf("v1/merchant/userLabel/deleteUserLabel/%d", int(userLabelId))).
+		obj = auth.DELETE(fmt.Sprintf("v1/merchant/userLabel/auto/deleteUserLabel/%d", int(userLabelId))).
 			Expect().Status(http.StatusOK).JSON().Object()
 		obj.Keys().ContainsOnly("status", "data", "message")
 		obj.Value("status").Number().Equal(200)
